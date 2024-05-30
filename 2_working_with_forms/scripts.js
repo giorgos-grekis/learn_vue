1abdc1e7b631e17ddc5553196a3e25b7169041ef
cisu <georgrekis@gmail.com>
2024-05-30 22:11:44 +0300

    two way data binding

---
+++ 2_working_with_forms/scripts.js
@@ -0,0 +1,45 @@
+const members = [
+    {
+        fname: 'John',
+        lname: 'Lennon',
+        instrument: 'Acoustic Guitar'
+    },
+    {
+        fname: 'George',
+        lname: 'Harrison',
+        instrument: 'Electric Guitar'
+    }
+]
+
+const handlingForms = {
+
+    data() {
+        return {
+            members: members,
+            newMember: {
+                fname: '',
+                lname: '',
+                instrument: ''
+            }
+        }
+    },
+
+    methods: {
+        addMember: function() {
+            if(this.newMember.fname && this.newMember.lname && this.newMember.instrument) {
+                this.members.push(this.newMember);
+                this.newMember = {
+                    fname: '',
+                    lname: '',
+                    instrument: ''
+                }
+            }
+            else {
+                alert("All fields must be filled.")
+            }
+        }
+    }
+
+};
+
+Vue.createApp(handlingForms).mount('#app');
