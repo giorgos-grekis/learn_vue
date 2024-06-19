588eb122b60179f26886fd9e34317b55ebfbef2b
cisu <georgrekis@gmail.com>
2024-06-19 23:04:41 +0300

    learn lifecycle hooks

--- 3_todo_list/scripts.js
+++ 3_todo_list/scripts.js
@@ -1,12 +1,12 @@
 const todos = [
-//   {
-//     text: "Learn HTML, CSS and Javascript",
-//     done: true,
-//   },
-//   {
-//     text: "Learn the basics of Vue JS",
-//     done: false,
-//   },
+  //   {
+  //     text: "Learn HTML, CSS and Javascript",
+  //     done: true,
+  //   },
+  //   {
+  //     text: "Learn the basics of Vue JS",
+  //     done: false,
+  //   },
 ];
 
 const todosApp = {
@@ -25,13 +25,25 @@ const todosApp = {
         this.todos.push(this.newTodo);
         this.newTodo = { done: false };
 
-        localStorage.setItem("vue_todos", JSON.stringify(this.todos))
-
+        localStorage.setItem("vue_todos", JSON.stringify(this.todos));
       } else {
-        alert("To-do test is required")
+        alert("To-do test is required");
       }
     },
   },
+
+  // Lifecycle Hooks
+  beforeCreate() {
+    console.log(this.newTodo);
+  },
+
+  created() {
+    console.log(this.newTodo);
+  },
+
+  //   beforeUpdate() {
+  //     console.log("Before update");
+  //   },
 };
 
 Vue.createApp(todosApp).mount("#app");
