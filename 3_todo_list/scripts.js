d1b9be0a118bdab30e293de4b61114bf2c65c782
cisu <georgrekis@gmail.com>
2024-06-19 22:19:35 +0300

    adding to-dos

--- 3_todo_list/scripts.js
+++ 3_todo_list/scripts.js
@@ -1,20 +1,34 @@
 const todos = [
-    {
-        text: 'Learn HTML, CSS and Javascript',
-        done: true
-    },
-    {
-        text: 'Learn the basics of Vue JS',
-        done: false
-    }
-]
+  {
+    text: "Learn HTML, CSS and Javascript",
+    done: true,
+  },
+  {
+    text: "Learn the basics of Vue JS",
+    done: false,
+  },
+];
 
 const todosApp = {
-    data() {
-        return {
-            todos
-        }
-    }
+  data() {
+    return {
+      todos,
+      newTodo: {
+        done: false,
+      },
+    };
+  },
+
+  methods: {
+    addTodo: function () {
+      if (this.newTodo.text) {
+        this.todos.push(this.newTodo);
+        this.newTodo = { done: false };
+      } else {
+        alert("To-do test is required")
+      }
+    },
+  },
 };
 
-Vue.createApp(todosApp).mount("#app")
+Vue.createApp(todosApp).mount("#app");
